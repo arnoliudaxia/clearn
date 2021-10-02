712d0cc25fda332bb3676939ddd40f110fe7880d
Liuyifei <liu20161225@qq.com>
2021-10-02 22:13:45 +0800

    双向链表是不存在的

--- clearn/yflinktable.h
+++ clearn/yflinktable.h
@@ -7,16 +7,9 @@ public:
 		int data;
 		Node* next;
 	};
-	struct BiNode
-	{
-		Node* last;
-		int data;
-		Node* next;
-	};
+
 	Node* initializeNode(int data);
-	Node* initializeBiNode(int data)
-	{
-	}
+
 	void logNodes(Node* start);
 	void addNode(int data, Node* head);
 	bool search(int param, Node* head);
